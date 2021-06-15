cb7f8deb732982260ca38ef61cc85fc4d57d723f
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-15 07:11:57 -0500

    Update photooxy.js

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -1,17 +1,18 @@
 let fetch = require('node-fetch')
 let split = '|'
-let handler = async (m, { conn, args: [effect], text: txt }) => {
+let handler = async (m, { conn, usedPrefix, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/photooxy')))).json()
-  if(!effect) throw 'effect not found'
+  if(!effect) throw `efecto no encontrado`
   effect = effect.toLowerCase()
-  if (!effect) throw `Efecto *${effect}* extraviado`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
   text2 = text2.join(split)
   let url = global.API('xteam', '/photooxy/' + effect, { text, text2 }, 'APIKEY')
-  if(!text) throw 'Ingresa el texto\n\nEjemplo: *.photooxy wolfmetal Sexy*\n            *.photooxy battlefield Sexy|bot*'
-  await conn.sendFile(m.chat, url, 'photooxy.jpg', `*PHOTOOXY*\n*Efecto:* ${effect}`, m)
+  if(!text) throw `Formato Incorrecto\n\nEjemplo:\n*${usedPrefix}wolfmetal sexy*\n*${usedPrefix}battlefield sexy|bot*`
+  conn.fakeReply(m.chat, '*ESPERA ⏳*', '0@s.whatsapp.net', '✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨')
+  await conn.sendFile(m.chat, url, 'photooxy.jpg', `✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨\n\n*Efecto:* ${effect}`, m)
+  .catch(() => { conn.reply(m.chat, `_Error! Formato incorrecto o inténtalo más tarde_`, m) })
 }
-handler.help = ['photooxy'].map(v => v + ' *efecto* _texto|[texto2]_')
+handler.help = ['photooxy'].map(v => v + ' *(efecto)* *texto|texto2*')
 handler.tags = ['tools']
 handler.command = /^(photooxy)$/i
 handler.register = true
