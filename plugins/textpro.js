8b88f199736fdd9779b880cb4e8ac17203b44d30
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-15 07:09:30 -0500

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -1,18 +1,20 @@
 let fetch = require('node-fetch')
 let split = '|'
 if(!split) throw 'effect not found'
-let handler = async (m, { conn, args: [effect], text: txt }) => {
+let handler = async (m, { conn, args: [effect], usedPrefix, text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*Lista de Effectos*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*Lista de Efectos*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
-  if (!effect in effects) throw `Efecto *${effect}* extraviado`
+  if (!effect in effects) throw `Efecto *${effect}* no encontrado`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
   text2 = text2.join(split)
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
-  if(!text) throw 'Ingresa el texto\n\nEjemplo: *.textpro jokerlogo Sexy*\n            *.textpro ninjalogo Sexy|bot*'
-  await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effecto:* ${effect}`, m)
+  if(!text) throw `Formato Incorrecto\n\nEjemplo:\n*${usedPrefix}neon sexy*\n*${usedPrefix}marvel xexy|bot*`
+  conn.fakeReply(m.chat, '*ESPERA ⏳*', '0@s.whatsapp.net', '✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨')
+  await conn.sendFile(m.chat, url, 'textpro.jpg', `✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨\n\n*Efecto:* ${effect}`, m)
+  .catch(() => { conn.reply(m.chat, `_Error! Formato incorrecto o inténtalo más tarde_`, m) })
 }
-handler.help = ['textpro'].map(v => v + ' *effect* _texto|[texto2]_')
+handler.help = ['textpro'].map(v => v + ' *(efecto)* *texto|texto2*')
 handler.tags = ['tools']
 handler.command = /^(textpro)$/i
 handler.register = true
