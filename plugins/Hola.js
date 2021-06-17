5dbfeaee631e7a8c789cf0b86e95a988c08f3cb5
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-17 16:15:54 -0500

    Update Hola.js

--- plugins/Hola.js
+++ plugins/Hola.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Hola que tal como va tu dia ??* , *Que tengas un exelente dia,tarde o noche... y si vas a dormir descansa ma�ana es un nuevo inicio*')
+let handler = m => m.reply('*Hola👋 que quel como va tu dia ??* , *Que tengas un exelente dia,tarde o noche... y si vas a dormir descansa mañana es un nuevo inicio🌚*')
 
 handler.customPrefix = /hola|Hola|holaa/i
 handler.command = new RegExp
