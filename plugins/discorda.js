c5a3d3bf97093d82ce31ee4ea1bdd6f84e430003
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 15:42:30 -0500

    Delete discorda.js

--- plugins/discorda.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*Aqui te dejo el link del discord* , *https://discord.gg/Zen6DQeVT6*')
-
-handler.customPrefix = /.discord|.discord|.discord /i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
