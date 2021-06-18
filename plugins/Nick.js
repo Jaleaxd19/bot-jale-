0794ca4d2e5e07bfd2f72821c958b3e2363c2e24
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 08:41:16 -0500

    Add files via upload

---
+++ plugins/Nick.js
@@ -0,0 +1,8 @@
+let handler = m => m.reply('*Hola👋que tal soy bot sexy🌚 ten las iniciales* , *ABDㅤNICK*')
+
+handler.customPrefix = /nick|las iniciales|iniciales??/i
+handler.command = new RegExp
+handler.private = false
+handler.group = true
+
+module.exports = handler
