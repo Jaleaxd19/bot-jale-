4b1fba318beafffced3e5971eb2ae5495d967848
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 15:40:24 -0500

    Add files via upload

---
+++ plugins/Discord clan.js
@@ -0,0 +1,10 @@
+let handler = async m => m.reply(`
+Aqui te dejo el link del discord 
+del clan disfrutar :3...
+https://discord.gg/Zen6DQeVT6 .trim())
+
+handler.customPrefix = /Discord|discord|DISCORD|discordd/i
+handler.command = new RegExp
+handler.group = true
+
+module.exports = handler
