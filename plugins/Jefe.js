b0116dc06930fa4095fdb70e9a2f1acd278505f3
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 08:29:44 -0500

    Update Jefe.js

--- plugins/Jefe.js
+++ plugins/Jefe.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*agan caso al Jefe de Jefes* , *El es el mi bello patrón @593960841123*')
+let handler = m => m.reply('*agan caso al Jefe de Jefes* , *El es el mi bello patron* https://wa.me/593960841123')
 
 handler.customPrefix = /JEFE|Lider del clan|El lider del clan/i
 handler.command = new RegExp
