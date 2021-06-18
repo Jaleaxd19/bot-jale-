0cf2e24701938f06628a806530b7215f3ad89ce6
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 08:22:49 -0500

    Add files via upload

--- plugins/Nick.js
+++ plugins/Nick.js
@@ -1,6 +1,6 @@
-let handler = m => m.reply('*Hola👋 que tal soy bot sexy ten las iniciales* , *ABDㅤNICK*')
+let handler = m => m.reply('*agan caso al Jefe de Jefes* , *El es el mi bello patrón @593960841123*')
 
-handler.customPrefix = /iniciales|nick|Iniciales/i
+handler.customPrefix = /JEFE|Lider del clan|El lider del clan/i
 handler.command = new RegExp
 handler.private = false
 handler.group = true
