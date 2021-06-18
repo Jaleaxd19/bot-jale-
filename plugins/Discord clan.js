29798bc2f1674439ffb7f8d5b990a59ae6e339f8
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-18 15:49:17 -0500

    Update Discord clan.js

--- plugins/Discord clan.js
+++ plugins/Discord clan.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`
 Aqui te dejo el link del discord 
 del clan disfrutar :3...
-https://discord.gg/Zen6DQeVT6 .trim())
+https://discord.gg/Zen6DQeVT6.trim())
 
-handler.customPrefix = /Discord|discord|DISCORD|discordd/i
+handler.customPrefix = /Discord|discord|discorrd|discordd/i
 handler.command = new RegExp
 handler.group = true
 
