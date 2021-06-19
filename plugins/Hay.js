15ff34280020bee60e8ece60569897ddfc921adc
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-19 11:46:53 -0500

    Update Hay.js

--- plugins/Hay.js
+++ plugins/Hay.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, '.mp3', m)
+       conn.sendFile(m.chat, 'media/Hay.mp3', m)
 }
 handler.help = ['sai']
 handler.tags = ['premium']
