7b1344e307063e92aa5ad8ca8f4aa798b205fdd1
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-20 18:07:27 -0500

    Update Hola.js

--- plugins/Hola.js
+++ plugins/Hola.js
@@ -1,6 +1,6 @@
 let handler = m => m.reply('*Hola👋 soy bot sexy 🌚??* , *En que te puedo ayudar¿?*_Escribe_ *#help* o *#Menú* _para que puedas ver mis funciones_ .*')
 
-handler.customPrefix = /Hola|Hola|Hola/i
+handler.customPrefix = /Hola/i
 handler.command = new RegExp
 handler.private = false
 handler.group = true
