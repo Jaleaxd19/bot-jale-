f89154ef5de3b0bf6b924b1daa25aad68552049c
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-20 19:04:15 -0500

    Update Nick.js

--- plugins/Nick.js
+++ plugins/Nick.js
@@ -1,6 +1,6 @@
 let handler = m => m.reply('*Hola👋que tal soy bot sexy🌚 ten las iniciales* ,    *ABDㅤNICK*')
 
-handler.customPrefix = / ^(#nick)$/i
+handler.customPrefix = /^(nick)$/i
 handler.command = new RegExp
 handler.private = false
 handler.group = true
