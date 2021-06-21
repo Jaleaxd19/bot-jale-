86c85f54197dbf27e4a261069d30c2bb4d06dea1
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-20 19:20:03 -0500

    Delete Buenosdias.js

--- plugins/Buenosdias.js
+++
@@ -1,9 +0,0 @@
-let handler = m => m.reply('_*Buen dia👋😘.*_
-_✨Te deseo un excelente inicio de dia✨_')
-
-handler.customPrefix = /Buenos días|buenos dias/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
