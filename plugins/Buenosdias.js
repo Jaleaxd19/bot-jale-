772f60806aff088da71489af33d83209094a85e4
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-21 06:21:55 -0500

    Update Buenosdias.js

--- plugins/Buenosdias.js
+++ plugins/Buenosdias.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Buen dia.*👋☺️😘, ✨_Te deseo un excelente inicio de dia_✨')
+let handler = m => m.reply('*Buen dia.*👋☺️😘, _✨Te deseo un excelente inicio de dia✨_ ')
 
 handler.customPrefix = /Buenos días|buenos dias/i
 handler.command = new RegExp
