99fa0ed1b06ae44c149181631bf5e979dfab17b9
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-23 14:06:13 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,6 +2,7 @@ let fs = require('fs')
 let path = require('path')
 let levelling = require('../lib/levelling')
 let tags = {
+let =conn.fakeReply(m.chat,  '*ESPERA 🛸*', '0@s.whatsapp.net', '*✨bot-sexy✨*')
   'main': 'Main',
   'game': 'Game',
   'xp': 'Exp & Limit',
