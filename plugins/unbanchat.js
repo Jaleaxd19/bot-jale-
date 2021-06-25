1c82aab2a45e9f9b60946d0d6ea8865cd6f73822
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-25 10:06:04 -0500

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
-  m.reply('Done!')
+  m.reply('Bot de regreso al grupo!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
