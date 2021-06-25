7df0326e1f6f8c589302e2deb66cd510496948ea
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-25 09:50:16 -0500

    Update Menu.js

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let path = require('path')
 let levelling = require('../lib/levelling')
 let tags = {
-  '*menu*': '*Menu*',
+  'main': 'Main',
   'game': 'Game',
   'xp': 'Exp & Limit',
   'sticker': 'Sticker',
