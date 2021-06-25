cf9600609109d228d97718f4de8394677bd3262f
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-25 10:15:11 -0500

    Update groupOpenClose.js

--- plugins/groupOpenClose.js
+++ plugins/groupOpenClose.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, args, usedPrefix }) => {
         conn.reply(m.chat, `*Grupo abierto con éxito!*`, m)
         conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
     } else if (args[0] === 'tutup') {
-        conn.reply(m.chat, `*Grupo cerrado con éxito!*`, m)
+        conn.reply(m.chat, `*Solo los *admin* cara fachera facherita pueden escribir!*`, m)
         conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
     } else {
         throw `*Ejemplo : ${usedPrefix}group open*`
