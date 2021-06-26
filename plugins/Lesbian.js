a9203391dfdea4142d0c681d0192299c73d87e37
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-26 12:19:02 -0500

    Create Lesbian.js

---
+++ plugins/Lesbian.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+
+ let handler = async(m, { conn }) => {
+
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/lesbians')
+
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+
+  }
+
+handler.help = ['lesbian']
+
+handler.tags = ['images']
+
+handler.command = /^(lesbian)$/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = true
+
+handler.group = false
+
+handler.private = false
+
+handler.admin = false
+
+handler.botAdmin = false
+
+handler.fail = null
+
+handler.exp = 0
+
+handler.limit = true
+
+module.exports = handler
