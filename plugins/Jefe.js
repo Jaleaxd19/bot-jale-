80be2dac5fb24b3b980219c40950c3f7fdd2d1c5
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-06-26 12:41:36 -0500

    Delete Jefe.js

--- plugins/Jefe.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*agan caso al Jefe de Jefes* , *El es el mi bello patron* https://wa.me/593960841123')
-
-handler.customPrefix = /JEFE|Lider del clan|El lider del clan/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
