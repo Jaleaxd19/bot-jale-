45fb11adf812bb9cd623bb510e1b0c59a60c1992
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-07-04 10:19:31 -0500

    Create Amigos.js

---
+++ plugins/Amigos.js
@@ -0,0 +1,33 @@
+let handler  = async (m, { conn }) => {
+
+       conn.sendFile(m.chat, 'media/Amigos.mp4', '', 'Xd', m)
+
+}
+
+handler.help = ['ami']
+
+handler.tags = ['premium']
+
+handler.command = /^(ami)$/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = false
+
+handler.private = false
+
+handler.admin = true
+
+handler.botAdmin = false
+
+handler.fail = null
+
+handler.limit = false
+
+handler.exp = 100
+
+module.exports = handler
