09b507babd63f9dd7b2b21f01b4308afe162bf34
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-07-04 08:07:47 -0500

    Create How.js

---
+++ plugins/How.js
@@ -0,0 +1,45 @@
+let handler = async (m, { conn, command, text }) => {
+
+  if (!text) throw `Siapa yang *${command.replace('how', '').toUpperCase()}*`
+
+  conn.reply(m.chat, `
+
+${command} *${text}*
+
+*${text}* es *${Math.floor(Math.random() * 101)}*% ${command.replace('how', '').toUpperCase()}
+
+`.trim(), m, m.mentionedJid ? {
+
+    contextInfo: {
+
+      mentionedJid: m.mentionedJid
+
+    }
+
+  } : {})
+
+}
+
+handler.help = ['gay', 'grilla', 'inteligente', 'guapo', 'bulgar', 'presumido', 'lesbiana', 'estresado', 'puerco', 'llorona', 'chicotriste'].map(v => 'how' + v + ' @nombre')
+
+handler.tags = ['kerang']
+
+handler.command = /^how(gay|grilla|inteligente|guapo|bulgar|presumido|lesbiana|estresado?|puerco|llorona|chicotriste)/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = false
+
+handler.private = false
+
+handler.admin = false
+
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
