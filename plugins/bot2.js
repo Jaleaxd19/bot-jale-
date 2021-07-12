d61436797ddde597b9ab83b3ed59f05be20e4bd8
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-07-12 12:13:26 -0500

    Add files via upload

---
+++ plugins/bot2.js
@@ -0,0 +1,8 @@
+let handler = m => m.reply('conn.senFile(m.chat......, m)')
+
+handler.customPrefix = /bot|botsito|bott/i
+handler.command = new RegExp
+handler.private = false
+handler.group = true
+
+module.exports = handler
