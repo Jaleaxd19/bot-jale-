6f1f48587c4ce264772f039581f68aad29c70fc2
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-07-12 08:23:00 -0500

    Delete Bot.js

--- plugins/Bot.js
+++
@@ -1,9 +0,0 @@
-let handler = m => m.reply('*Si... si estoy aqui.*👺 , *¿En que te puedo ayudar? Escribe_ *.?* o *.menu* _para que puedas ver mis funciones._*')
-conn.senFile(m.chat......, m)
-
-handler.customPrefix = /^(bot)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
