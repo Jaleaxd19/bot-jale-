76b6a78a23467ffe387600878f214f18133c6cb5
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-07-15 14:30:33 -0500

    Add files via upload

---
+++ plugins/serverv2rayt.js
@@ -0,0 +1,14 @@
+let handler = async m => m.reply(`
+  
+   ╭─────❲ *✨𝒃𝒐𝒕-𝒔𝒆𝒙𝒚✨* ❳──────╮
+vmess://eyJhZGQiOiJ1c2EyLnYyLXJheS5jb20iLCJhaWQiOiIzMiIsImlkIjoiMzg4NjRiM2UtMjk2Yi00NzA1LTg0YjQtNzNjMjQxOWQ3NzQyIiwiaG9zdCI6Inh4ZmFzdC53aGF0c2FwcC5uZXQiLCJuZXQiOiJ3cyIsInBhdGgiOiIvZmFzdHNzaD91PWxlb25lbDc2NSZwPTYwZWY2ZTIxMWZiMGQmIiwicG9ydCI6IjQ0MyIsInBzIjoidXNhMi52Mi1yYXkuY29tIiwidGxzIjoidGxzIiwidHlwZSI6Im5vbmUiLCJ2IjoiMiJ9
+╰─────❲ _Formato_ *[Vmess]* ❳─────╯
+*COPIA Y PEGA EN LA APP*
+*solo Tuenti*
+❐ _Activa_ _el_ *[true]* _en_ ➣AllowInsecure ,no olvides agradecer no seas rata_ ❐          
+`.trim()) 
+handler.help = ['V2ray|v2ray']
+handler.tags = ['servidores']
+handler.command = /^V2ray$/i
+
+module.exports = handler
