5bc3b03aedff4740393ca51ed2fe9155cb2edf90
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-08-03 15:26:50 -0500

    Add files via upload

---
+++ plugins/canalbk.js
@@ -0,0 +1,17 @@
+let handler = async m => m.reply(`
+╭────❲ *ＢＬＯＯＤＫＩＬＬＥＲＳ益* ❳────╮
+├┉┉┉┉↯❏ *Discord:* ❏↯┉┉┉
+https://discord.gg/KWQRdgytCA                         
+├┉┉┉┉↯❏  *Instagram del clan:* ❏↯┉┉┉┉
+https://instagram.com/blood.killers19?utm_medium=copy_link
+
+╰────❲ *✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨* ❳────╯
+`.trim())
+
+handler.customPrefix = /#bkcanal|#bkcanal|#bkcanal|#bkcanal/i
+
+handler.command = new RegExp
+
+handler.group = true
+
+module.exports = handler
