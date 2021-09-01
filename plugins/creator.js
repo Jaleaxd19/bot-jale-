427e9b73720b34840fc9c503a5d9afa348042ef2
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:22:13 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '593 96 084 1123', 'Kevin', m)
-  this.sendContact(m.chat, '593960841123', 'Kevin', m)
+  // this.sendContact(m.chat, '1 360 559 7043', 'Nita', m)
+  this.sendContact(m.chat, '13605597043', 'Nita', m)
 }
 handler.help = ['owner', 'creador']
 handler.tags = ['info']
