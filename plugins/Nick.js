535280bf1a52e76aa29717d0ccd56213e4548c6e
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:08:50 -0500

    Delete Nick.js

--- plugins/Nick.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*Hola👋que tal soy bot sexy🌚 ten las iniciales* ,    *ABDㅤNICK*')
-
-handler.customPrefix = /^(nick)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
