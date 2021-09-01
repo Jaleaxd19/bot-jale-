da5b42c45452777b323b03830586e44fdfb9c239
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:08:30 -0500

    Update Menu.js

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -51,7 +51,7 @@ ${'```%npmdesc```'}
 `,
 }
 let handler = async (m, { conn, usedPrefix: _p }) => {
-  conn.fakeReply(m.chat,  '*ESPERA 👿*', '0@s.whatsapp.net', '*✨bot-sexy✨*')
+  conn.fakeReply(m.chat,  '*ESPERA ⏳...*', '0@s.whatsapp.net', '*✨𝒃𝒐𝒕-𝒋𝒂𝒍𝒆𝒂✨*')
   conn.sendFile(m.chat, 'media/menu.opus', m)
   try {
     let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
