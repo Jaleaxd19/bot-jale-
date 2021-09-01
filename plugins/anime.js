78861f803dbe6469bf7a9fcef7a6808769dcceb4
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:16:03 -0500

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, args, command, usedPrefix }) => {
   fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/random.txt').then(res => res.text()).then(body => {
     let randomnime = body.split('\n')
     let randomnimex = randomnime[Math.floor(Math.random() * randomnime.length)]
-    conn.sendFile(m.chat, randomnimex, '', '✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨', m)
+    conn.sendFile(m.chat, randomnimex, '', '✨𝒃𝒐𝒕-𝒋𝒂𝒍𝒆𝒂✨', m)
   }).catch(() => {
     conn.reply(m.chat, ` !`, m)
   })
