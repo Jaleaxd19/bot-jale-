4d2d7fb3d0a5d1b90f6e8f0993ae02d498019377
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:52:35 -0500

    Update nako.js

--- plugins/nako.js
+++ plugins/nako.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Nyaa *BY:* ✨𝒃𝒐𝒕-𝒔𝒆𝒙𝒚✨', m)
+  conn.sendFile(m.chat, json.url, '', 'Nyaa *BY:* ✨𝒃𝒐𝒕-𝒋𝒂𝒍𝒆𝒂✨', m)
 }
 handler.help = ['neko']
 handler.tags = ['internet']
