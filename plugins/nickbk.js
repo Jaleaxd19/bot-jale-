40e0545101dcce7a1a965a541609ec8855c7fed2
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:54:26 -0500

    Update nickbk.js

--- plugins/nickbk.js
+++ plugins/nickbk.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Hola👋que tal soy bot sexy🌚 ten las iniciales* ,    *ᴮᴷ︱*')
+let handler = m => m.reply('*Hola👋que tal soy 𝒃𝒐𝒕-𝒋𝒂𝒍𝒆𝒂🌚 ten las iniciales* ,    *ᴮᴷ︱*')
 
 handler.customPrefix = /^(nick0)$/i
 handler.command = new RegExp
