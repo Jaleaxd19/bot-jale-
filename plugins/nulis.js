3a98054b8597032631b6dd5287f2580346c5a6e9
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-01 16:55:54 -0500

    Update nulis.js

--- plugins/nulis.js
+++ plugins/nulis.js
@@ -51,7 +51,7 @@ let handler  = async (m, { conn, args }) => {
   ])
   .on('error', e => conn.reply(m.chat, util.format(e), m))
   .on('exit', () => {
-    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Made by CristianC593', m)
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Made by Jalea', m)
   })
 }
 handler.help = ['n'].map(v => v + 'ulis <teks>')
