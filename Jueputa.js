054fcb809b35798a103f8ffe15e1bedc329fd77d
Jaleaxd19 <89426989+Jaleaxd19@users.noreply.github.com>
2021-09-02 06:20:32 -0500

    Update Jueputa.js

--- Jueputa.js
+++ Jueputa.js
@@ -7,12 +7,12 @@ handler.before = function (m, { user, bot, groupMetadata }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('*Adiós no queremos patanes,rompiste las reglas serás exterminado....!!*')
+    m.reply('*AdiÃ³s no queremos patanesðŸ‘º,rompiste las reglas serÃ¡s exterminado....!!*')
   m.reply('*tienes 3 segundos para retractarte!!!!!!*')
     m.reply('*3!!*')
     m.reply('*2!!*')
     m.reply('*1!!*')
-if (user.isAdmin || user.isSuperAdmin) return m.reply('*Te salvaste cagón eres admin, no puedo eliminarte ptm :v*')
+if (user.isAdmin || user.isSuperAdmin) return m.reply('*Te salvaste cagÃ³n eres adminðŸ˜ , no puedo eliminarte ptm :v*')
     let participants = m.isGroup ? groupMetadata.participants : []
     let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {}
     if (bot.isAdmin || bot.isSuperAdmin) {
